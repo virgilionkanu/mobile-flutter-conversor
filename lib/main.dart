548ee6c6267754e4aadaef7257c535476feaa36f
Virgílio Cano <virgiliocano53@gmail.com>
2019-05-11 13:06:07 -0300

    Adicionando, Melhorando os TextField, e Definindo um estilo para os textos

--- lib/main.dart
+++ lib/main.dart
@@ -12,38 +12,60 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    AppBar appBar = AppBar(
+      title: Text("Conversor de Temperatura"),
+      centerTitle: true,
+      backgroundColor: Colors.blue,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: () {},
+        )
+      ],
+    );
+
+    Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow,);
+
+    TextStyle styleDecoration = TextStyle(color: Colors.blueAccent, fontSize: 20);
+    TextStyle styleField = TextStyle(color: Colors.blueAccent);
+
+    TextField tempCelsius = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: "Temperatura em graus Celsius",
+        labelStyle: styleDecoration,
+      ),
+      textAlign: TextAlign.center,
+      style: styleField,
+    );
+
+    TextField tempFahrenheit = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: "Temperatura em graus Fahrenheit",
+        labelStyle: styleDecoration
+      ),
+      textAlign: TextAlign.center,
+      style: styleField,
+    );
+
+    Column column = Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        icon,
+        tempCelsius,
+        tempFahrenheit,
+      ],
+    );
+
+    Scaffold scaffold = Scaffold(
+      appBar: appBar,
+      backgroundColor: Colors.white,
+      body: column,
+    );
+
+    return scaffold;
   }
 }
 
-Widget build(BuildContext context) {
-  AppBar appBar = AppBar(
-    title: Text("Conversor de Temperatura"),
-    centerTitle: true,
-    backgroundColor: Colors.blue,
-    actions: <Widget>[
-      IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: () {},
-      )
-    ],
-  );
-  
-  Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow,);
-  //
 
-  Column column = Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        icon,
-      ],
-  );
-
-  Scaffold scaffold = Scaffold(
-    appBar: appBar,
-    backgroundColor: Colors.white,
-    body: column,
-  );
-
-  return scaffold;
-}
